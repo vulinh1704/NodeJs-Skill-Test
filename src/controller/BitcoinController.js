80d9146e3cd70932999bdcb5029a02f63b563f44
vulinh1704 <truongdanglinh1704@gmail.com>
2024-03-15 15:31:14 +0700

    done project - 15/03 - Vu Linh

--- src/controller/BitcoinController.js
+++ src/controller/BitcoinController.js
@@ -7,6 +7,12 @@ class BitcoinController {
 
     getBitcoin = async (req, res) => {
         let bitcoin = await this.bitcoinService.getLatestBTCData();
+        bitcoin = {
+            mins: bitcoin.mins,
+            price: bitcoin.price,
+            closeTime: bitcoin.closeTime,
+            timeAt: bitcoin.timeAt
+        }
         return res.status(200).json(bitcoin);
     }
 }
